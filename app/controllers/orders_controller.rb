972c97d0dc14559b4120b2bcba803d2850dd24aa
Karacay Deniz <OCA2602IT0021@edu.oca.ac.jp>
2026-09-25 06:15:03 +0000

    Initial commit (Rails setup)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController < ApplicationController
   end
 
   def index
-    @orders = current_user.orders.all
+    @orders = current_user.orders.order(created_at: :desc)
   end
 
   def confirm
@@ -35,31 +35,38 @@ class OrdersController < ApplicationController
     end
   end
 
-  def create
-    @order = current_user.orders.build(order_params)
+def create
+  @order = Order.new(order_params)
+  @order.user_id = current_user.id
 
-    if @order.save
-      redirect_to complete_order_path(@order)
-    else
-      @product = Product.find(@order.product_id)
-      render :new
+  if @order.save
+    current_user.cart.cart_items.each do |item|
+      OrderDetail.create(
+        order_id: @order.id,
+        product_id: item.product.id,
+        quantity: item.quantity,
+        price: item.product.price
+      )
     end
-  end
 
-  def complete
-    @order = current_user.orders.find(params[:id])
-    @product = @order.product
-  end
+    current_user.cart.cart_items.destroy_all
 
+    redirect_to complete_order_path(@order)
+  else
+    redirect_to new_order_path, alert: "注文内容に誤りがあります。"
+  end
+end
+
+def complete
+  @order = Order.find(params[:id])
+end
   private
 
-  def order_params
-    params.require(:order).permit(
-      :product_id,
-      :count,
-      :address,
-      :total_price
-    )
-  end
+ def order_params
+  params.require(:order).permit(
+    :address,
+    :total_price
+  )
+end
 
 end
